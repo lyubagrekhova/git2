4f830d1d79b4560ea5f7bc4f5e8805bdb939e577
lyubagrekhova <lyubagrekhova18@gmail.com>
2024-12-28 13:35:53 +0900

    Исправлен код

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,9 @@
-name = input("Введите ваше имя: ")
-print("Hello world from " + name)
+def greet_user():
+    name = input("Введите ваше имя: ").strip()
+    if name: 
+        print(f"Hello world from {name}")
+    else:
+        print("Имя не может быть пустым. Пожалуйста, введите ваше имя.")
+
+if __name__ == "__main__":
+    greet_user()
